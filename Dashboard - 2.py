5998c97b22996f9fcadda0d96fc0687e31c0f3a6
daniyar135 <94024241+daniyar135@users.noreply.github.com>
2022-04-19 14:10:47 -0500

    Add files via upload

--- Dashboard - 2.py
+++ Dashboard - 2.py
@@ -4,45 +4,24 @@ import plotly.express as px
 import pandas as pd
 import os
 
-df1 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/Training Hours VS Pass Rate.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/daniyar135/flam/main/Training%20Hours%20VS%20Pass%20Rate.csv')
 app = Dash(__name__)
 server = app.server
+
+fig = px.bar(df, y="Pass Rate", x="Courses", barmode="group")
 
 app.layout = html.Div(children=[
     html.H1(children='FLAM Dashboard'),
 
     html.Div(children='''
-        Visualizing data from our longest SunVault related calls. The longer the call, the bigger the shape.
-    '''),
-    html.Div(children='''
-        Use the slider at the bottom to see each week's longest calls that had silences > 2 minutes.
+        Visualizing data from our SPRI warehouses
     '''),
 
-    dcc.Graph(id='graph-with-slider'),
-
-    dcc.Slider(
-        df1['week'].min(),
-        df1['week'].max(),
-        step=None,
-        value=df1['week'].min(),
-        marks={str(week): str(week) for week in df1['week'].unique()},
-        id='week-slider'
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
     )
 ])
 
-@app.callback(
-    Output('graph-with-slider', 'figure'),
-    Input('week-slider', 'value'))
-def update_figure(selected_week):
-    filtered_df = df1[df1.week == selected_week]
-
-    fig = px.scatter(filtered_df, x="Number of silences", y="Call duration", size="Call duration", color="Recording ID",
-                     log_x=True, size_max=55)
-
-    fig.update_layout(transition_duration=500)
-
-    return fig
-
 if __name__ == '__main__':
     app.run_server()
-# %%
