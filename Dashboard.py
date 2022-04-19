d92483a1fb0533eb6862d74ce61ad5d780bab522
daniyar135 <94024241+daniyar135@users.noreply.github.com>
2022-04-19 15:13:36 -0500

    Add files via upload

---
+++ Dashboard.py
@@ -0,0 +1,27 @@
+# %%Dash v2
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import pandas as pd
+import os
+
+df = pd.read_csv('https://raw.githubusercontent.com/daniyar135/flam/main/Training%20Hours%20VS%20Pass%20Rate.csv')
+app = Dash(__name__)
+server = app.server
+
+fig = px.scatter(df, y="Pass Rate", x="Group Courses Taken", color="Warehouse Name")
+
+app.layout = html.Div(children=[
+    html.H1(children='FLAM Dashboard'),
+
+    html.Div(children='''
+        Visualizing data from our SPRI warehouses
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server()
