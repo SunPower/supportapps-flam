f7a73b8949de83a23cb5b8ef73cf81cb10ea660a
daniyar135 <94024241+daniyar135@users.noreply.github.com>
2022-04-20 10:41:20 -0500

    Add files via upload

--- Dashboard.py
+++ Dashboard.py
@@ -5,23 +5,36 @@ import pandas as pd
 import os
 
 df = pd.read_csv('https://raw.githubusercontent.com/daniyar135/flam/main/Training%20Hours%20VS%20Pass%20Rate.csv')
+df1 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/flam/main/Training%20Hours%20VS%20Pass%20Rate%20Dealer.csv')
 app = Dash(__name__)
 server = app.server
 
-fig = px.scatter(df, y="Pass Rate", x="Group Courses Taken", color="Warehouse Name")
+fig = px.scatter(df, y="Pass Rate", x="Group Courses Taken", color="Warehouse Name", size="Group Courses Taken")
+fig1 = px.scatter(df1, y="Pass Rate", x="Courses Taken", color="Dealer Name", size="Courses Taken")
 
 app.layout = html.Div(children=[
     html.H1(children='FLAM Dashboard'),
 
     html.Div(children='''
-        Visualizing data from our SPRI warehouses
+        Visualizing data from our Dealer Network and SPRI warehouses
+    '''),
+
+    html.Div(children='''
+        Hover over a dot to see which SPRI/Dealer it represents, number of courses taken, and average pass rate
     '''),
 
     dcc.Graph(
-        id='example-graph',
+        id='spri-graph',
         figure=fig
+    ),
+
+    dcc.Graph(
+        id='dealer-graph',
+        figure=fig1
     )
 ])
 
 if __name__ == '__main__':
     app.run_server()
+
+# %%
