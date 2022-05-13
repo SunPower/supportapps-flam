2e8f2d56e4c2ce1f780c2b68c5b175825a4f0b52
daniyar135 <94024241+daniyar135@users.noreply.github.com>
2022-05-12 22:18:04 -0500

    Add files via upload

--- Dashboard.py
+++ Dashboard.py
@@ -4,13 +4,15 @@ import plotly.express as px
 import pandas as pd
 import os
 
-df = pd.read_csv('https://raw.githubusercontent.com/daniyar135/supportapps-flam/main/SPRI.csv')
-df1 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/supportapps-flam/main/Dealer.csv')
+#df = pd.read_csv('https://raw.githubusercontent.com/daniyar135/supportapps-flam/main/SPRI.csv')
+#df1 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/supportapps-flam/main/Dealer.csv')
+df1 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/supportapps-flam/main/All.csv')
 app = Dash(__name__)
 server = app.server
 
-fig = px.scatter(df, y="Pass Rate", x="Courses Taken", color="Code", size="Courses Taken", trendline="ols")
-fig1 = px.scatter(df1, y="pass_rate", x="courses_taken", color="Code", size="Number of Audits", trendline="ols")
+#fig = px.scatter(df, y="Pass Rate", x="Courses Taken", color="Code", size="Courses Taken", trendline="ols")
+#fig1 = px.scatter(df1, y="pass_rate", x="courses_taken", color="Code", size="Number of Audits", trendline="ols")
+fig = px.scatter(df1, y="pass_rate", x="courses_taken", color="Name", size="Number of Inspections", trendline="ols")
 
 app.layout = html.Div(children=[
     html.H1(children='FLAM Dashboard'),
@@ -24,14 +26,14 @@ app.layout = html.Div(children=[
     '''),
 
     dcc.Graph(
-        id='spri-graph',
+        id='all-graph',
         figure=fig
     ),
 
-    dcc.Graph(
-        id='dealer-graph',
-        figure=fig1
-    ),
+    # dcc.Graph(
+    #     id='spri-graph',
+    #     figure=fig
+    # ),
 
     html.Div(children='''
         Adjust this slider for pass rate
@@ -42,19 +44,19 @@ app.layout = html.Div(children=[
         df1['pass_rate'].max(),
         step=None,
         value=df1['pass_rate'].max(),
+        tooltip={"placement": "bottom", "always_visible": True},
         id='passrate-slider'
     )
 ])
 
 @app.callback(
-    Output('dealer-graph', 'figure'),
+    Output('all-graph', 'figure'),
     Input('passrate-slider', 'value'))
 
 def update_figure(selected_passrate):
     filtered_df = df1[df1.pass_rate <= selected_passrate]
 
-    fig = px.scatter(filtered_df, x="courses_taken", y="pass_rate", color="Code", size="Number of Audits", trendline="ols",
-                     log_x=True, size_max=55)
+    fig = px.scatter(filtered_df, x="courses_taken", y="pass_rate", color="Name", size="Number of Inspections", trendline="ols")
     fig.update_layout(transition_duration=500)
 
     return fig
